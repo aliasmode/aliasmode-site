9c520334952832d608b327448e16b2fbd8dcb875
dtheDavid <115281730+dtheDavid@users.noreply.github.com>
2026-09-01 22:56:48 +0000

    Remove typecheck probe

--- src/data/typecheck-probe.ts
+++
@@ -1,5 +0,0 @@
-import { contentPages } from './content-index';
-import type { ContentPage } from './content/types';
-const check: ContentPage[] = contentPages;
-// force use
-export const total = check.length;
